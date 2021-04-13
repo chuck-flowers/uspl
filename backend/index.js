0dc552b38e717c421be7a5e1b6130776069e06f9
Timothy Flowers <t_chuck_flowers@yahoo.com>
2021-04-13 17:32:01 -0400

    Adds the route that returns the array of orders

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,33 @@
 const express = require('express');
+const https = require('https');
 
 const app = express();
+
+async function getOrdersFromExternalApi(success, failure) {
+    https.get('https://api-ext.gouspack.com/api/orders', response => {
+        var jsonData = '';
+
+        response.on('data', text => {
+            jsonData += text;
+        });
+
+        response.on('end', () => {
+            const orders = JSON.parse(jsonData);
+            success(orders);
+        });
+    }).on('error', failure);
+}
+
+app.get('/orders', async (_, res) => {
+    getOrdersFromExternalApi(orders => {
+        res.json(orders).end();
+    }, ex => {
+        res.status(500)
+            .send('There was a problem fetching the orders data as requested:\n' + ex)
+            .end();
+    });
+});
+
 app.listen(8081, () => {
     console.log("Backend running at http://localhost:8081");
 });
