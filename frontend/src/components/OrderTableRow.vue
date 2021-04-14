f639af448d786dd193521ae1f1f70119349ed133
Timothy Flowers <t_chuck_flowers@yahoo.com>
2021-04-13 20:51:55 -0400

    Adds proper date formatting

--- frontend/src/components/OrderTableRow.vue
+++ frontend/src/components/OrderTableRow.vue
@@ -1,10 +1,10 @@
 <template>
   <tr>
     <td>{{ orderNumber }}</td>
-    <td>{{ orderDate }}</td>
+    <td>{{ formattedOrderDate }}</td>
     <td>{{ orderStatus }}</td>
-    <td>{{ scheduledDateTime }}</td>
-    <td>{{ deliveredDateTime }}</td>
+    <td>{{ formattedScheduledDateTime }}</td>
+    <td>{{ formattedDeliveredDateTime }}</td>
   </tr>
 </template>
 <script>
@@ -12,7 +12,60 @@ export default {
   data() {
     return {};
   },
-  methods: {},
+  methods: {
+    padNumber(number, minDigits) {
+      var numberStr = number.toString();
+      const originalLength = numberStr.length;
+      for (var i = 0; i < minDigits - originalLength; i++) {
+        numberStr = "0" + numberStr;
+      }
+
+      return numberStr;
+    },
+    formatDate(date) {
+      if (date === null) {
+        return "";
+      }
+
+      const year = this.padNumber(date.getFullYear(), 4);
+      const month = this.padNumber(date.getMonth() + 1, 2);
+      const day = this.padNumber(date.getDate(), 2);
+      return `${year}-${month}-${day}`;
+    },
+    formatDateTime(date) {
+      const hour = this.padNumber(date.getHours() % 12, 2);
+      const minute = this.padNumber(date.getMinutes(), 2);
+      const amPm = date.getHours() > 12 ? "PM" : "AM";
+
+      return `${this.formatDate(date)} ${hour}:${minute} ${amPm}`;
+    },
+  },
+  computed: {
+    formattedOrderDate() {
+      if (this.orderDate === null) {
+        return "";
+      }
+
+      const date = new Date(this.orderDate);
+      return this.formatDate(date);
+    },
+    formattedScheduledDateTime() {
+      if (this.scheduledDateTime === null) {
+        return "";
+      }
+
+      const date = new Date(this.scheduledDateTime);
+      return this.formatDateTime(date);
+    },
+    formattedDeliveredDateTime() {
+      if (this.deliveredDateTime === null) {
+        return "";
+      }
+
+      const date = new Date(this.deliveredDateTime);
+      return this.formatDateTime(date);
+    },
+  },
   props: {
     orderNumber: String,
     orderDate: String,
